0ba713b5942896b4434628c066d2ca8beb39fadc
aman <amangupta2786@gmail.com>
2021-08-30 23:05:41 +0530

    Updates

---
+++ precache-manifest.6528e6d309fcaf98ee8aaf3dfff01fae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3389f849b1f602737df631677cdbad",
+    "url": "/testrepo/index.html"
+  },
+  {
+    "revision": "ca1a1678a4556faafde2",
+    "url": "/testrepo/static/js/2.86be280e.chunk.js"
+  },
+  {
+    "revision": "0297a3bf3b23d1141b94ac92cf395022",
+    "url": "/testrepo/static/js/2.86be280e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63383f5b59af61c3a9cc",
+    "url": "/testrepo/static/js/main.42a44d9c.chunk.js"
+  },
+  {
+    "revision": "25da394336d05b738233",
+    "url": "/testrepo/static/js/runtime-main.a1e80c61.js"
+  }
+]);
